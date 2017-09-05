9f9ff1e2e7c61512deec7f782abe8e715b763d60
cesine <cesine@yahoo.com>
2017-09-04 20:51:51 -0400

    WIP building

--- app/src/main/java/com/github/fielddb/lessons/MainActivity.java
+++ app/src/main/java/com/github/fielddb/lessons/MainActivity.java
@@ -3,6 +3,8 @@ package com.github.fielddb.lessons;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.opensourcefieldlinguistics.fielddb.speech.kartuli.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
